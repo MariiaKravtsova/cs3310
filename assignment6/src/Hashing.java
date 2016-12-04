a37f2edf33372c52794f353f5d6282f52cbcba1a
MariiaKravtsova <kravtsova.mariia@gmail.com>
2016-12-04 17:15:59 -0500

    Add completed A6, not perfect yet

--- assignment6/src/Hashing.java
+++ assignment6/src/Hashing.java
@@ -1,23 +1,135 @@
 import java.util.ArrayList;
-
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 /**
  *
  */
 public class Hashing {
 
-    private static final int SIZE = 659;
+    // Satisfies the requirement of working with billion of data from the start
+    private static final int SIZE = 2000000;
 
-    ArrayList<String> studentList = new ArrayList<String>(SIZE);
+    private ArrayList<String> studentList = new ArrayList<String>(SIZE);
+    private File studentFile = new File("studentsData.txt");
 
-    public void add(String studentname) {
+    public void createFile() throws FileNotFoundException {
+
+        boolean created = false;
+        Scanner reader;
+
+        if (studentFile.exists()) {
+            reader = new Scanner(studentFile);
+            while (reader.hasNext()){
+                studentList.add(reader.next());
+            }
+            reader.close();
+        } else {
+            try {
+                created = studentFile.createNewFile();
+                if(created){
+                    buildArray();
+                    System.out.println("File created successfully.");
+                }
+                else{
+                    System.out.println("File cannot be created.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void buildArray(){
+        int i;
+
+        for (i = 0; i < SIZE; i++) {
+            studentList.add(i, "null");
+        }
+    }
+
+    public void saveFile() {
+        try {
+            FileWriter writer = new FileWriter(studentFile);
+            for(String str: studentList) {
+                writer.write(str + System.lineSeparator());
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void add(String studentName) {
         int hash;
         int i;
 
         hash = 7;
-        for (i = 0; i < studentname.length(); i++) {
-            hash = hash^studentname.charAt(i);
+        for (i = 0; i < studentName.length(); i++) {
+            hash = hash^studentName.charAt(i);
         }
         System.out.print(hash);
-        //studentList.add(hash, studentname);
+        studentList.set(hash, studentName);
+    }
+
+    public void findByValue(int hashValue) {
+
+        if (!studentList.get(hashValue).equals("null")) {
+            System.out.println("At value " + hashValue + " we can find " + studentList.get(hashValue));
+        } else {
+            System.out.println("Record doesn't exist.");
+        }
+    }
+
+    public void findByName(String studentName) {
+        int hash;
+        int i;
+
+        hash = 7;
+        for (i = 0; i < studentName.length(); i++) {
+            hash = hash^studentName.charAt(i);
+        }
+        if (!studentList.get(hash).equals("null")) {
+            System.out.println(studentName + "'s hash value is at " + studentList.get(hash));
+        } else {
+            System.out.println("Record doesn't exist.");
+        }
+    }
+
+    public void deleteByName(String studentName) {
+        int hash;
+        int i;
+
+        hash = 7;
+        for (i = 0; i < studentName.length(); i++) {
+            hash = hash^studentName.charAt(i);
+        }
+        if (!studentList.get(hash).equals("null")) {
+            studentList.set(hash, "null");
+            System.out.println("Record deleted.");
+        } else {
+            System.out.println("Record doesn't exist.");
+        }
+    }
+
+    public void deleteByValue(int hashValue) {
+        if (!studentList.get(hashValue).equals("null")) {
+            studentList.set(hashValue, "null");
+            System.out.println("Record deleted.");
+        } else {
+            System.out.println("Record doesn't exist.");
+        }
+    }
+
+    public void printAll() {
+        int i;
+        for (i = 0; i < SIZE; i++) {
+            if (!studentList.get(i).equals("null")){
+                System.out.println(i + " " + studentList.get(i));
+            }
+        }
     }
 }
--- assignment6/src/Main.java
+++ assignment6/src/Main.java
@@ -1,57 +1,74 @@
-import javax.imageio.IIOException;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.io.File;
-import java.io.IOException;
 
 public class Main {
 
-    public ArrayList studentList = new ArrayList();
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         int choice;
+        int value;
+        String name;
         Scanner user_input = new Scanner(System.in);
 
         Hashing hashing = new Hashing();
 
         choice = 0;
 
-//        try {
-//            File studentsFile = new File("names.txt");
-//
-//            if (!studentsFile.exists()) {
-//                studentsFile.createNewFile();
-//
-//        } catch(IOException e){
-//              System.err.println("Problem writing to the file statsTest.txt");
-//        }
+        hashing.createFile();
 
-        while (choice != 5) {
+        while (choice != 7) {
             System.out.println();
             System.out.println("----------- MENU ------------");
             System.out.println("1. Add a student");
-            System.out.println("2. Find a student");
-            System.out.println("3. Delete a student");
-            System.out.println("4. Print all students");
-            System.out.println("5. Exit");
+            System.out.println("2. Find a student by name");
+            System.out.println("3. Find a student by hash value");
+            System.out.println("4. Delete a student by name");
+            System.out.println("5. Delete a student by hash value");
+            System.out.println("6. Print all students");
+            System.out.println("7. Exit");
             System.out.println();
             System.out.println("Please enter a number from the menu: ");
             choice = Integer.parseInt(user_input.next());
 
             switch (choice) {
                 case 1:
-                    hashing.add("Ana");
+                    System.out.println();
+                    System.out.println("Please enter a name to add: ");
+                    name = user_input.next();
+                    hashing.add(name);
                     break;
                 case 2:
+                    System.out.println();
+                    System.out.println("Please enter a name to find: ");
+                    name = user_input.next();
+                    hashing.findByName(name);
                     break;
                 case 3:
+                    System.out.println();
+                    System.out.println("Please enter a value to look up: ");
+                    value = Integer.parseInt(user_input.next());
+                    hashing.findByValue(value);
                     break;
                 case 4:
+                    System.out.println();
+                    System.out.println("Please enter a name to delete: ");
+                    name = user_input.next();
+                    hashing.deleteByName(name);
+                    break;
+                case 5:
+                    System.out.println();
+                    System.out.println("Please enter a value to delete: ");
+                    value = Integer.parseInt(user_input.next());
+                    hashing.deleteByValue(value);
+                    break;
+                case 6:
+                    hashing.printAll();
                     break;
             }
 
 
         }
+
+        hashing.saveFile();
     }
 
 }
